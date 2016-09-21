ca01d4a626c22aee5679b4662a3f31e281da258e
lingmm <450044101@qq.com>
2016-09-21 12:03:43 +0800

    fix

--- dist/index.js
+++ dist/index.js
@@ -5,7 +5,7 @@ const sso_1 = require('./lib/sso');
 const enterprise_1 = require('./lib/enterprise');
 module.exports = {
     Suite: suite_1.default,
-    SuiteCallback: suite_callback_1.default,
+    SuiteCallBack: suite_callback_1.default,
     SSO: sso_1.default,
     Enterprise: enterprise_1.default
 };
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import Suite from './lib/suite'
-import SuiteCallback from './lib/suite_callback'
+import SuiteCallBack from './lib/suite_callback'
 import SSO from './lib/sso'
 import Enterprise from './lib/enterprise'
 
 export = {
   Suite,
-  SuiteCallback,
+  SuiteCallBack,
   SSO,
   Enterprise
 };
